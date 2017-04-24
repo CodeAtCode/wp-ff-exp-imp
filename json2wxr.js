ac1aade3f34b7d687ff31fc9a5ba4679062903e8
Daniele Scasciafratte <mte90net@gmail.com>
2017-04-24 21:29:13 +0200

    finally the wxr

--- json2wxr.js
+++ json2wxr.js
@@ -2,7 +2,9 @@
 var Importer = require('wxr');
 var fs = require('fs');
 var importer = new Importer();
-var obj = JSON.parse(fs.readFileSync('./json-result/export.json', 'utf8'));
+var json_file = fs.readFileSync('./json-result/export.json', 'utf8');
+var json_file = json_file.replace(/<script[^>]*>.*?<\/script>/gi,'').replace(/<blockquote[^>]*>.*?<\/blockquote>/gi,'')
+var obj = JSON.parse(json_file);
 
 var wpcat = {}
 
@@ -33,7 +35,7 @@ for (var i = 0; i < obj.length; ++i) {
 	status: obj[i].post_status,
 	post: obj[i].post_type,
 	author: "wp_user_replace",
-	contentEncoded: obj[i].post_content.replace('[', '<<').replace(']', '>>'),
+	contentEncoded: obj[i].post_content.trim(),
 	categories: cat_post,
 	tags: tag_post
   });
@@ -51,7 +53,6 @@ for (var i = 0; i < obj.length; ++i) {
 }
 
 var file_xml = importer.stringify();
-file_xml = file_xml.replace('[', '<=').replace(']', '=>');
 
 fs.writeFile("./json-result/export.xml", file_xml, function (err) {
   if (err) {
--- single-json.sh
+++ single-json.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 jq -s '.' ./export-json/*.json > ./json-result/export.json
+sed -i 's/\[poll id=\\"1\\"\]//g' ./json-result/export.json
